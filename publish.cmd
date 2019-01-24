d531efdf017c4f6b808089bb90716291f7a41d8b
Venkata Sambasiva Rao Kesanam <Venkata.Rao@intertecsys.com>
2019-01-24 17:34:51 +0400

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=dubaipolicechatb-Web-Deploy.pubxml -p:Password=8wPYyvqo3eae8bC9ncSYrxWpk3tXff14zp3xseQm6xGEX4qWlmQj4jwrBP3L
+
